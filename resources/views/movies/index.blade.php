dbb3b7c415fb64c2d6369509b886447458e725f0
Corfaya <lauracorrado0696@gmail.com>
2024-09-25 14:35:18 +0200

    [UPD] MovieController controller added and routes updated

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    benvenuti
+    CARDS dei film
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController as PageController;
+use App\Http\Controllers\MovieController as MovieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('homepage');
+Route::get('/', [PageController::class, 'index'])->name('homepage');
+Route::get('/movies', [MovieController::class, 'index'])->name('movies');
